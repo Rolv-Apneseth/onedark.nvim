6bf96f38c74add86b5226cf968eaae748dd352b1
rolv <rolv.apneseth@gmail.com>
2022-10-22 14:08:36 +0100

    Modify some lualine specific highlihgts

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -1,4 +1,4 @@
-local c = require('onedark.colors')
+local c = require("onedark.colors")
 local cfg = vim.g.onedark_config
 local colors = {
     bg = c.bg0,
@@ -9,24 +9,24 @@ local colors = {
     blue = c.blue,
     purple = c.purple,
     cyan = c.cyan,
-    gray = c.grey
+    gray = c.grey,
 }
 
 local one_dark = {
     inactive = {
-        a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
-        b = {fg = colors.gray, bg = colors.bg},
-        c = {fg = colors.gray, bg = cfg.transparent and c.none or c.bg1},
+        a = { fg = colors.gray, bg = colors.bg, gui = "bold" },
+        b = { fg = colors.gray, bg = colors.bg },
+        c = { fg = colors.gray, bg = cfg.transparent and c.none or c.bg1 },
     },
     normal = {
-        a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-        b = {fg = colors.fg, bg = c.bg3},
-        c = {fg = colors.fg, bg = cfg.transparent and c.none or c.bg1},
+        a = { fg = colors.fg, bg = colors.purple, gui = "bold" },
+        b = { fg = colors.fg, bg = c.bg3 },
+        c = { fg = colors.fg, bg = c.none },
     },
-    visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
-    replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
-    insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
-    command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}},
-    terminal = {a = {fg = colors.bg, bg = colors.cyan, gui = 'bold'}},
+    --[[ visual = { a = { fg = colors.fg, bg = colors.purple, gui = "bold" } }, ]]
+    --[[ replace = { a = { fg = colors.fg, bg = colors.red, gui = "bold" } }, ]]
+    --[[ insert = { a = { fg = colors.fg, bg = colors.yellow, gui = "bold" } }, ]]
+    --[[ command = { a = { fg = colors.fg, bg = colors.blue, gui = "bold" } }, ]]
+    --[[ terminal = { a = { fg = colors.bg, bg = colors.cyan, gui = "bold" } }, ]]
 }
-return one_dark;
+return one_dark
